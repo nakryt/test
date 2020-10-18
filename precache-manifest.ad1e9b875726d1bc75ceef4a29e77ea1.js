222cb27da3a72a4af2cc2af23f97edf96e8dc3f5
Volodymyr Pestov <pestov.v@yahoo.com>
2020-10-18 21:05:07 +0300

    Updates

---
+++ precache-manifest.ad1e9b875726d1bc75ceef4a29e77ea1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a112f41d93e54507fe1d16db188281ed",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "a7ed21b6eb07147a70f5",
+    "url": "/test/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "2fdbf52a832e93116fb2",
+    "url": "/test/static/css/main.ae735e54.chunk.css"
+  },
+  {
+    "revision": "a7ed21b6eb07147a70f5",
+    "url": "/test/static/js/2.c9ae230c.chunk.js"
+  },
+  {
+    "revision": "857d129e4a5f4f33c7a4a143bf79109c",
+    "url": "/test/static/js/2.c9ae230c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fdbf52a832e93116fb2",
+    "url": "/test/static/js/main.586b2eb6.chunk.js"
+  },
+  {
+    "revision": "c8c489537cd82e5155a8",
+    "url": "/test/static/js/runtime-main.cecc38e4.js"
+  },
+  {
+    "revision": "7e4a385628297502feea3f1ca349e8f9",
+    "url": "/test/static/media/banner-large.7e4a3856.webp"
+  },
+  {
+    "revision": "6d4ba55a93b43f77752539c1f98e7e1b",
+    "url": "/test/static/media/banner-medium.6d4ba55a.webp"
+  },
+  {
+    "revision": "235c9422dffa278fdd8b861c805f9840",
+    "url": "/test/static/media/banner-small.235c9422.webp"
+  },
+  {
+    "revision": "2a97abf4622d0657fa2bdb0bd888cdfc",
+    "url": "/test/static/media/banner-xlarge.2a97abf4.webp"
+  },
+  {
+    "revision": "faa97c72ce500f38fad57e18e9f9b153",
+    "url": "/test/static/media/logo.faa97c72.svg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/test/static/media/man-laptop-v1.3cdf7897.svg"
+  },
+  {
+    "revision": "6f6bef288009ad20ca333f0cd61608c6",
+    "url": "/test/static/media/menu-icon.6f6bef28.svg"
+  },
+  {
+    "revision": "070f998a128aab988c263cfba22e4fd1",
+    "url": "/test/static/media/photo-cover.070f998a.svg"
+  }
+]);
